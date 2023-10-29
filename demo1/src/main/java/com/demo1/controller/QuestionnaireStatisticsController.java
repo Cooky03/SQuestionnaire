7d8e6482d8dd0763181ca76d5d3fc765c52f8eea
Cooky03 <876364906@qq.com>
2023-10-29 14:05:22 +0800

    first commit

---
+++ demo1/src/main/java/com/demo1/controller/QuestionnaireStatisticsController.java
@@ -0,0 +1,37 @@
+//package com.demo1.controller;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.gyb.questionnaire.config.RequiredLogin;
+//import com.gyb.questionnaire.dto.QuestionnaireStatisticDTO;
+//import com.gyb.questionnaire.service.QuestionnaireStatisticsService;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//
+//
+//@Controller
+//public class QuestionnaireStatisticsController {
+//    private final QuestionnaireStatisticsService statisticsService;
+//
+//    public QuestionnaireStatisticsController(QuestionnaireStatisticsService statisticsService) {
+//        this.statisticsService = statisticsService;
+//    }
+//
+//    @GetMapping("/questionnaire/statistics/{questionnaireId}")
+//    @RequiredLogin
+//    public String analysisAndStatistics(@PathVariable Long questionnaireId,
+//                                        Model m){
+//        final QuestionnaireStatisticDTO dto = statisticsService.statistic(questionnaireId);
+//        m.addAttribute("dto",dto);
+//        try {
+//            final ObjectMapper objectMapper = new ObjectMapper();
+//            final String s = objectMapper.writeValueAsString(dto);
+//            m.addAttribute("json",s);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        return "questionnaire_statistics";
+//    }
+//
+//}
---
+++ demo1/src/main/resources/static/my-config.js
@@ -0,0 +1,2 @@
+
+const API_BASE_URL = 'http://172.22.8.245:8089'
